4ea8d4eb3aa697000253bf990544d04526903741
Baljeet <bal1@publicisgroupe.net>
2022-04-27 17:46:08 +0530

    header design completed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./header.styles.scss";
 
 const Index = () => {
-  return <div>HEADER</div>;
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          className="img-container"
+          src="static/images/logo.png"
+          alt="Sabka Bazaar Logo"
+        />
+        <div className="header__links">
+          <Link to="/">Home</Link>
+          <Link to="/product-list">Products</Link>
+        </div>
+      </div>
+      <div className="header__cartIcon">
+        <div className="header__options">
+          <Link to="/login">Sign In</Link>
+          <Link to="/register">Register</Link>
+        </div>
+        <div className="cart">
+          <div className="cartDisplayContent">
+            <img src="static/images/cart.svg" alt="Cart Icon" />
+            <span>0 Items</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Index;
